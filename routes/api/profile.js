a64c40f84b44c81299528c882683027777a1f64f
Abhishek <abhishekdevang18@gmail.com>
2019-10-22 21:01:03 +0530

    add education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -163,7 +165,7 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
-// @route  PUT api/profile experience
+// @route  PUT api/profile/experience
 // @desc   Add profile experience
 // @access Private
 router.put(
@@ -226,8 +228,8 @@ router.put(
   }
 );
 
-// @route  GET api/profile/experience/:exp_id
-// @desc   Get experience by experience ID
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete experience by experience ID
 // @access Private
 router.delete("/experience/:exp_id", auth, async (req, res) => {
   try {
@@ -248,5 +250,120 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+
+// @route  PUT api/profile/education
+// @desc   Add profile education
+// @access Private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required")
+        .not()
+        .isEmpty(),
+      check("degree", "Degree is required")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "Field of study is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    // Build Education object
+    const educationFields = {};
+    if (school) educationFields.school = school;
+    if (degree) educationFields.degree = degree;
+    if (fieldofstudy) educationFields.fieldofstudy = fieldofstudy;
+    if (from) educationFields.from = from;
+    if (to) educationFields.to = to;
+    if (current) educationFields.current = current;
+    if (description) educationFields.description = description;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (!profile) return res.status(400).send("Profile not found");
+
+      profile.education.unshift(educationFields);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+// @route  DELETE api/profile/education/:edu_id
+// @desc   Delete education by education ID
+// @access Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map(item => item._id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// @route  GET api/profile/github/:username
+// @desc   Get user repos from Github
+// @access Public
+router.get('/github/:username', (req,res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&cliend_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+    }
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !==200){
+        return res.status(404).json({msg: 'No Github profile found'});
+      }
+
+      res.json(JSON.parse(body));
+    })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+})
 
 module.exports = router;
