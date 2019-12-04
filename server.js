c4b4b2a99ee6c215597097d4d20cb5fba7027b10
Abhishek <abhishekdevang18@gmail.com>
2019-12-04 21:54:19 +0530

    test produ

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-//app.get("/", (req, res) => res.send("API Running"));
+//  app.get("/", (req, res) => res.send("API's Running"));
 
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
